fa73c94ac91fdd8254a0369cdf296d011e560a5f
Jojo Tutor <jeshrave003@gmail.com>
2018-04-04 17:12:21 +0800

    find by id

--- index.js
+++ index.js
@@ -62,6 +62,9 @@ async function queryData() {
     await
         r   .table('users')
             .run(connection, (err, cursor) => {
+                if (err) {
+                    throw err
+                }
                 cursor.toArray((err, result) => {
                     if (err) {
                         throw err
@@ -78,6 +81,9 @@ async function queryDataWithFilter() {
         r   .table('users')
             .filter(r.row('name').eq('John Doe'))
             .run(connection, (err, cursor) => {
+                if (err) {
+                    throw err
+                }
                 cursor.toArray((err, result) => {
                     if (err) {
                         throw err
@@ -88,13 +94,27 @@ async function queryDataWithFilter() {
 
 }
 
+async function queryDataById() {
+    console.log('@queryDataById')
+    await
+        r   .table('users')
+            .get('cc3f45f6-c8da-4cfb-a01c-7291e9368481')
+            .run(connection, (err, result) => {
+                if (err) {
+                    throw err
+                }
+                console.log(JSON.stringify(result, null, 2))
+            })
+}
+
 async function main() {
     console.log('@START')
     await connect()
     // await createTable()
     // await insertDataToTable()
-    await queryData()
-    await queryDataWithFilter()
+    // await queryData()
+    // await queryDataWithFilter()
+    await queryDataById()
     console.log('@END')
     process.exit(1)
 }
