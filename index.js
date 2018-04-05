9e7fd49d755faef8a3066d3bcf780b7b5c654f6f
Jojo Tutor <jeshrave003@gmail.com>
2018-04-05 18:27:49 +0800

    update data

--- index.js
+++ index.js
@@ -126,15 +126,32 @@ async function updateAllData() {
 
 }
 
+async function updateData() {
+    console.log('@updateData')
+    await
+        r   .table('users')
+            .filter(r.row('name')
+            .eq('Kelly Klarkson'))
+            .update({ age: 21 })
+            .run(connection, (err, result) => {
+                if (err) {
+                    throw err
+                }
+                console.log(JSON.stringify(result, null, 2))
+            })
+        
+}
+
 async function main() {
     console.log('@START')
     await connect()
     await feedListener()
     // await createTable()
-    await insertDataToTable()
+    // await insertDataToTable()
     // await queryData()
     // await queryDataWithFilter()
     // await queryDataById()
+    await updateData()
     console.log('@END')
     process.exit(1)
 }
